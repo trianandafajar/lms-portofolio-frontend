cf0a381efcb6c75e064bee5b3bae408b2add26fb
Triananda Fajar <mail.trianandafajar@gmail.com>
2025-11-17 13:42:35 +0700

    fix: change exec_mode to 'fork' in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     {
       name: "mentora-frontend",
       port: "3009",
-      exec_mode: "cluster",
-      instances: "max",
+      exec_mode: "fork",
       script: "./.output/server/index.mjs",
     },
   ],
